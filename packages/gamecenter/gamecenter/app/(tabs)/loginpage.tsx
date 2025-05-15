61e0a04e2a3f7512320cfa6e62e724bb5d6fed55
met <mehmetemretopal@gmail.com>
2025-05-15 13:52:50 +0300

    loginpage added

---
+++ packages/gamecenter/gamecenter/app/(tabs)/loginpage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function loginpage() {
+  return (
+    <View>
+      <Text>loginpage</Text>
+    </View>
+  )
+}
